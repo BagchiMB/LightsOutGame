40c1072e64cd4d2645aa7fe281944a996c4a0b8b
BagchiMB <mbagchi6884@gmail.com>
2020-05-21 22:18:57 +0530

    Update Readme

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
-# LightsOutGame
-Fork the Repository
-Open CMD
-Navigate to the folder
-type npm start
+# Lights Out Game
+## Objective is to turn off all the lights
 
-#dependencies
-Node
-npm
+## Technology
+1. React js
+2. Jsx
+
+## Rules
+On clicking on aparticular tile all the adjacent tiles in all direction toggles
+
+## You can find this project hosted on [http://lights-out-reactify.surge.sh/](http://lights-out-reactify.surge.sh/)
+
+# Screenshot
+![image](/screenshot/Screenshot73.png)
---
+++ build/precache-manifest.d1041b2d4f0dc21a887928911b147461.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d496a8197716cf84ea74",
+    "url": "/static/css/main.fc59fe2f.chunk.css"
+  },
+  {
+    "revision": "d496a8197716cf84ea74",
+    "url": "/static/js/main.d496a819.chunk.js"
+  },
+  {
+    "revision": "7fe92e0c27d5088f443c",
+    "url": "/static/js/1.7fe92e0c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d670d0aff8847ff6f64ef03c57f8546a",
+    "url": "/index.html"
+  }
+];
